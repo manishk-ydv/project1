ab670c7f0e121ef1651dccec64f320625e63c172
manish-yadav0 <manishmayor@gmail.com>
2025-02-01 01:25:01 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import connectdb from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+connectdb;
+
+then (()=>{
+    app.listen(process.env.PORT ||  8000 ,()=>{
+        console.log(`server is running in at port ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("Error not connected on db",err);
+});
